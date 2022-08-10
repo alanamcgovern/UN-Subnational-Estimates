8f270fc8498e7d72faf805ed19c6f36f1d9f3af7
alanamcgovern <amcgov@uw.edu>
2022-08-10 16:52:15 -0700

    8.10

--- UR_checks/UR_checks.R
+++ UR_checks/UR_checks.R
@@ -9,41 +9,51 @@ library(openxlsx)
 library(tidyverse)
 
 ## specify filepath
-filenames <- c('Angola/ang_urbhh_2015-16.xlsx', # add 2011 MIS
-               'Bangladesh/bgd_urbhh_2011.xlsx','Bangladesh/bgd_urbhh_2014.xlsx','Bangladesh/bgd_urbhh_2017-18.xlsx', #add 2019 MICS
-               'Benin/ben_urbhh_2011-12.xlsx', 'Benin/ben_urbhh_2017-18.xlsx', 
-               'Burkina_Faso/bfa_urbhh_2010.xlsx',
-               'Burundi/bdi_urbhh_2010.xlsx', 'Burundi/bdi_urbhh_2016-17.xlsx',
-               'Cameroon/cmr_urbhh_2011.xlsx',  'Cameroon/cmr_urbhh_2018.xlsx',
-               'Chad/tcd_urbhh_2014-15.xlsx',
-               'Cote_dIvoire/civ_urbhh_2011-12.xlsx',
-               'DR_Congo/cod_urbhh_2013-14.xlsx',
-               'Ethiopia/eth_urbhh_2011.xlsx','Ethiopia/eth_urbhh_2016.xlsx','Ethiopia/eth_urbhh_2019.xlsx',
-               'Ghana/gha_urbhh_2014.xlsx',
-               'Guinea/gin_urbhh_2012.xlsx','Guinea/gin_urbhh_2018.xlsx',
-               'Haiti/hti_urbhh_2012.xlsx','Haiti/hti_urbhh_2016-17.xlsx',
-               'India/ind_urbhh_2015-16.xlsx', # can't find frame/sample info for 2019-2021 survey
-               'Kenya/ken_urbhh_2014.xlsx',
-               'Lesotho/lso_urbhh_2014.xlsx', #add 2018 MICS  
-               'Liberia/lbr_urbhh_2013.xlsx', 'Liberia/lbr_urbhh_2019-20.xlsx',
-               # add MICS Madagascar
-               'Malawi/mwi_urbhh_2015-16.xlsx', # can't find frame households for 2010, add 2013-14 and 2019-20 MICS
-               'Mali/mli_urbhh_2012-13.xlsx','Mali/mli_urbhh_2018.xlsx',
-               'Mauritania/mrt_urbhh_2019-21.xlsx',
-               'Mozambique/moz_urbhh_2015.xlsx', # no adequate sample info for 2011
-               'Myanmar/mmr_urbhh_2015-16.xlsx',
-               'Nepal/npl_urbhh_2011.xlsx','Nepal/npl_urbhh_2016.xlsx',
-               'Niger/ner_urbhh_2012.xlsx')
-               # couldn't find sample frame for Nigeria (2010, 2013, 2018)
+filenames <- c('ang_urbhh.xlsx',
+               'bgd_urbhh.xlsx', #missing 2019 MICS -- oversampled anyway
+               'ben_urbhh.xlsx',
+               'bfa_urbhh.xlsx',
+               'bdi_urbhh.xlsx',
+               'cmr_urbhh.xlsx', 
+               'tcd_urbhh.xlsx',
+               'civ_urbhh.xlsx',
+               'cod_urbhh.xlsx',
+               'eth_urbhh.xlsx',
+               'gha_urbhh.xlsx',
+               'gin_urbhh.xlsx',
+               'hti_urbhh.xlsx',
+               'ind_urbhh.xlsx', # can't find frame/sample info for 2019-2021 survey*
+               'ken_urbhh.xlsx',
+               'lso_urbhh.xlsx', #missing 2018 MICS -- oversampled anyway
+               'lbr_urbhh.xlsx', 
+               'mdg_urbhh.xlsx',
+               'mwi_urbhh.xlsx', # can't find frame households for 2010, add 2013-14 and 2019-20 MICS -- oversampled anyway
+               'mli_urbhh.xlsx',
+               'mrt_urbhh.xlsx',
+               'moz_urbhh.xlsx', # no adequate sample info for 2011 -- oversampled anyway
+               'mmr_urbhh.xlsx',
+               'npl_urbhh.xlsx',
+               'ner_urbhh.xlsx',
+               'nga_urbhh.xlsx',  # couldn't find sample frame for 2010
+               'pak_urbhh.xlsx',
+               'rwa_urbhh.xlsx', # couldn't find sample household for 2010 -- oversampled anyway
+               'sen_urbhh.xlsx',
+               'sle_urbhh.xlsx', # add 2017 MICS -- oversampled anyway
+               'tgo_urbhh.xlsx',
+               'uga_urbhh.xlsx',
+               'tza_urbhh.xlsx', # no adequate infor for 2010 survey*
+               'zmb_urbhh.xlsx',
+               'zwe_urbhh.xlsx') 
 
 get_URdiff <- function(file){
   tab <- read.xlsx(paste0(file))
-  tab <- tab %>% mutate(diff = sample_prop-frame_prop,perc_diff = (sample_prop-frame_prop)/frame_prop)
+  tab <- tab %>% filter(is.na(frame_prop) + is.na(sample_prop)==0) %>% mutate(diff = sample_prop-frame_prop,perc_diff = (sample_prop-frame_prop)/frame_prop)
   if(sum(is.na(tab$f_urban) + is.na(tab$f_total) + is.na(tab$s_urban) + is.na(tab$s_total))==0){
-    tab <- tab %>% mutate(chisq = ifelse(frame_prop>0 & frame_prop<1, (s_urban-(s_total*f_urban/f_total))^2/(s_total*frame_prop*(1-frame_prop)),0))
-  }else{tab <- tab %>% mutate(chisq = NA)}
+    tab <- tab %>% mutate(chisq = ifelse(frame_prop>0 & frame_prop<1, (s_urban-(s_total*f_urban/f_total))^2/(s_total*frame_prop*(1-frame_prop)),0),
+                          chisq2 = ifelse(frame_prop>0 & frame_prop<1 & sample_prop>frame_prop, (s_urban-(s_total*f_urban/f_total))^2/(s_total*frame_prop*(1-frame_prop)),0))
+  }else{tab <- tab %>% mutate(chisq = NA,chisq2 = NA)}
                         
-  res <- c(nrow(tab),sum(tab$chisq),1-pchisq(sum(tab$chisq),nrow(tab)-1),median(tab$diff),tab$diff[which.max(abs(tab$diff))],median(tab$perc_diff),tab$perc_diff[which.max(abs(tab$perc_diff))])
+  res <- c(nrow(tab),sum(tab$chisq),1-pchisq(sum(tab$chisq),nrow(tab)-1),sum(tab$chisq2),1-pchisq(sum(tab$chisq2),nrow(tab)-1),median(tab$diff),tab$diff[which.max(abs(tab$diff))],median(tab$perc_diff),tab$perc_diff[which.max(abs(tab$perc_diff))])
   if(-min(tab$diff)>0.29){
     plot.min <- min(tab$diff) - 0.01
   }else{plot.min <- -0.3}
@@ -64,7 +74,7 @@ for(i in 2:length(filenames)){
 }
 
 row.names(comparisons) <- filenames
-colnames(comparisons) <- c('NumberofAreas','Chi-Square','p','MedianDiff','MaxDiff','MedianPercDiff','MaxPercDiff')
+colnames(comparisons) <- c('NumberofAreas','Chi-Square','p','Chi-Square2','p2','MedianDiff','MaxDiff','MedianPercDiff','MaxPercDiff')
 
 comparisons
 
