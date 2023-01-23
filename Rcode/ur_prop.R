ed29bdce83b8b8a35aad7ed33fbcf5f8816c1875
Jessica Godwin <jlg0003@uw.edu>
2023-01-23 12:29:43 -0800

    Senegal ur_prop.R and sen_2013_frame_urb_prop.csv

--- Rcode/ur_prop.R
+++ Rcode/ur_prop.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 # ENTER COUNTRY OF INTEREST AND YEAR OF SAMPLING FRAME (must be in frame_years)  -----------------------------------------------
 # Please capitalize the first letter of the country name and replace " " in the country name to "_" if there is.
-country <- "Mauritania"
+country <- "Senegal"
 frame_year <- 2013
 
 # Load libraries and Info -----------------------------------------------
